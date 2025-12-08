01432cb9c0a3cbe2c0d9debf2d13e37fab7d165e
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-12-09 01:01:41 +0530

    callback and setTimeout 02

--- script.js
+++ script.js
@@ -14,4 +14,32 @@ abcd(function(fn){
   fn(function(fn3){
       fn3();
   });
-});
+}); 
+
+// callbacks
+// situation -> github se repo data laao
+
+// getUserDetails(username, cb)
+// getAllRepos(userid, cb)
+// getRepoDetail(repos[0], cb)
+
+
+// amitSeDetailsLaao(address, cb)
+// dukaanKoDhundho(details, cb)
+// samaanLelo(samaanlist, cb)
+// gharAaajaao(address, cb)
+
+ function tanishkSeDetailsLaao(name, cb){
+  console.log("fetching details...")
+  setTimeout(() =>{
+    cb({ rollno1: 58 , rollno2: 59});
+  }, 3000)
+ }
+
+ tanishkSeDetailsLaao("Tanishk Rajak", function
+  (details) {
+    console.log(details);
+  });
+
+
+ 
