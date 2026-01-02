05533be5a036aab66a760518b9d362571b620763
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2026-01-02 13:36:00 +0530

    Callback Concepts and use

---
+++ IntroductionToAsynchronous-in-js/CallbackHell-CallBackUse.js
@@ -0,0 +1,52 @@
+// introduction to Asyncronous code
+
+// callback hell -- the early use of 'callback functions' 
+
+function abcd(fn){
+  fn(function(fn2){
+    fn2(function () {
+      console.log("hye");
+    });
+  });
+}
+
+abcd(function(fn){
+  fn(function(fn3){
+      fn3();
+  });
+}); 
+
+// callbacks
+// situation -> github se repo data laao
+
+// getUserDetails(username, cb)
+// getAllRepos(userid, cb)
+// getRepoDetail(repos[0], cb)
+
+
+// amitSeDetailsLaao(address, cb)
+// dukaanKoDhundho(details, cb)
+// samaanLelo(samaanlist, cb)
+// gharAaajaao(address, cb)
+
+ function tanishkSeDetailsLaao(name, cb){
+  console.log("fetching details...")
+  setTimeout(() =>{
+    cb({ rollno1: 58 , rollno2: 59});
+  }, 3000)
+ }
+
+ tanishkSeDetailsLaao("Tanishk Rajak", function
+  (details) {
+    console.log(details);
+  });
+
+function instagramParJaaoDataLaao(username, cb) {
+  // logic
+  cb({ id: 1, username: "harsh", age: 27 });
+}
+
+instagramParJaaoDataLaao("harsh", function(data){
+  console.log(data);
+});
+ 
